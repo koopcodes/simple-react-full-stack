b7381eecb862565fe6cdff40b461eb19a1ff5f2d
koopdev <koop@koop.codes>
2018-10-17 16:47:01 -0700

    Server working

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const os = require('os');
+const http = require('http');
 
 const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+const server = http.createServer(app);
+
+app.set('port', port);
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+server.on('listening', () => {
+  console.log(`server is listening for requests on port ${server.address().port}`);
+});
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.get('/marco', (req, res) => res.send('polo'));
